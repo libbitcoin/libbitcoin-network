ac1e0899abfe80a434e6a46ddab524c2c867bba4
evoskuil <eric@voskuil.org>
2016-02-04 02:57:18 -0800

    Build secp256k1 dependnecy.

--- install.sh
+++ install.sh
@@ -224,6 +224,12 @@ BOOST_OPTIONS_CLANG=\
 "--with-system "\
 "--with-test "
 
+# Define secp256k1 options.
+#------------------------------------------------------------------------------
+SECP256K1_OPTIONS=\
+"--disable-tests "\
+"--enable-module-recovery "
+
 # Define bitcoin options.
 #------------------------------------------------------------------------------
 BITCOIN_OPTIONS=\
@@ -548,6 +554,7 @@ build_from_travis()
 build_all()
 {
     build_from_tarball_boost $BOOST_URL $BOOST_ARCHIVE boost $PARALLEL $BOOST_OPTIONS
+    build_from_github libbitcoin secp256k1 version4 $PARALLEL "$@" $SECP256K1_OPTIONS
     build_from_github libbitcoin libbitcoin master $PARALLEL "$@" $BITCOIN_OPTIONS
     build_from_travis libbitcoin libbitcoin-network master $PARALLEL "$@" $BITCOIN_NETWORK_OPTIONS
 }
