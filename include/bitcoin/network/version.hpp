79e742178d3da53441f50051e109c8cc73c40c20
evoskuil <eric@voskuil.org>
2017-02-16 04:48:54 -0800

    Bump master branch from version 3.0.0 to version 4.0.0. (reverted from commit 38a41f1f612cba058357c03d9228f37380fa4eb2)

--- include/bitcoin/network/version.hpp
+++ include/bitcoin/network/version.hpp
@@ -12,8 +12,8 @@
  * For interpretation of the versioning scheme see: http://semver.org
  */
 
-#define LIBBITCOIN_NETWORK_VERSION "4.0.0"
-#define LIBBITCOIN_NETWORK_MAJOR_VERSION 4
+#define LIBBITCOIN_NETWORK_VERSION "3.0.0"
+#define LIBBITCOIN_NETWORK_MAJOR_VERSION 3
 #define LIBBITCOIN_NETWORK_MINOR_VERSION 0
 #define LIBBITCOIN_NETWORK_PATCH_VERSION 0
 
