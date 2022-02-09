765a8d4d4c82dac9cb5af187934ad8d3b4ea094a
evoskuil <eric@voskuil.org>
2022-02-08 23:39:58 -0800

    Add acceptor and connector tests.

--- test/net/acceptor.cpp
+++ test/net/acceptor.cpp
@@ -17,13 +17,121 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "../test.hpp"
-#include <bitcoin/network.hpp>
 
 BOOST_AUTO_TEST_SUITE(acceptor_tests)
 
-BOOST_AUTO_TEST_CASE(acceptor)
+class accessor
+  : public acceptor
 {
-    BOOST_REQUIRE(true);
+public:
+    using acceptor::acceptor;
+
+    const settings& get_settings() const
+    {
+        return settings_;
+    }
+
+    const asio::io_context& get_service() const
+    {
+        return service_;
+    }
+
+    const asio::strand& get_strand() const
+    {
+        return strand_;
+    }
+
+    bool get_stopped() const
+    {
+        return stopped_;
+    }
+
+    deadline::ptr get_timer()
+    {
+        return timer_;
+    }
+
+    asio::acceptor& get_acceptor()
+    {
+        return acceptor_;
+    }
+};
+
+BOOST_AUTO_TEST_CASE(acceptor__construct__default__expected_properties)
+{
+    threadpool pool(1);
+    asio::strand strand(pool.service().get_executor());
+    const settings set(bc::system::chain::selection::mainnet);
+    auto instance = std::make_shared<accessor>(strand, pool.service(), set);
+
+    BOOST_REQUIRE(&instance->get_settings() == &set);
+    BOOST_REQUIRE(&instance->get_service() == &pool.service());
+    BOOST_REQUIRE(&instance->get_strand() == &strand);
+    BOOST_REQUIRE(instance->get_stopped());
+    BOOST_REQUIRE(instance->get_timer());
+    BOOST_REQUIRE(!instance->get_acceptor().is_open());
+
+    pool.stop();
+    pool.join();
+}
+
+BOOST_AUTO_TEST_CASE(acceptor__start__always__expected)
+{
+    threadpool pool(1);
+    asio::strand strand(pool.service().get_executor());
+    const settings set(bc::system::chain::selection::mainnet);
+    auto instance = std::make_shared<acceptor>(strand, pool.service(), set);
+    instance->start(42);
+    pool.stop();
+    pool.join();
+}
+
+BOOST_AUTO_TEST_CASE(acceptor__accept__timeout__channel_timeout)
+{
+    threadpool pool(2);
+    asio::strand strand(pool.service().get_executor());
+    settings set(bc::system::chain::selection::mainnet);
+    set.connect_timeout_seconds = 0;
+    auto instance = std::make_shared<acceptor>(strand, pool.service(), set);
+    BOOST_REQUIRE(!instance->start(42));
+
+    boost::asio::post(strand, [&]()
+    {
+        instance->accept([](const code& ec, channel::ptr channel)
+        {
+            BOOST_REQUIRE_EQUAL(ec, error::channel_timeout);
+            BOOST_REQUIRE(!channel);
+        });
+    });
+
+    pool.stop();
+    pool.join();
+}
+
+BOOST_AUTO_TEST_CASE(acceptor__accept__stop__operation_canceled)
+{
+    threadpool pool(2);
+    asio::strand strand(pool.service().get_executor());
+    settings set(bc::system::chain::selection::mainnet);
+    set.connect_timeout_seconds = 1000;
+    auto instance = std::make_shared<acceptor>(strand, pool.service(), set);
+    BOOST_REQUIRE(!instance->start(42));
+
+    boost::asio::post(strand, [&]()
+    {
+        instance->accept([](const code& ec, channel::ptr channel)
+        {
+            BOOST_REQUIRE_EQUAL(ec, error::operation_canceled);
+            BOOST_REQUIRE(!channel);
+        });
+
+        // Test race.
+        std::this_thread::sleep_for(microseconds(1));
+        instance->stop();
+    });
+
+    pool.stop();
+    pool.join();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- test/net/connector.cpp
+++ test/net/connector.cpp
@@ -17,13 +17,145 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "../test.hpp"
-#include <bitcoin/network.hpp>
 
 BOOST_AUTO_TEST_SUITE(connector_tests)
 
-BOOST_AUTO_TEST_CASE(connector)
+class accessor
+  : public connector
 {
-    BOOST_REQUIRE(true);
+public:
+    using connector::connector;
+
+    const settings& get_settings() const
+    {
+        return settings_;
+    }
+
+    const asio::io_context& get_service() const
+    {
+        return service_;
+    }
+
+    const asio::strand& get_strand() const
+    {
+        return strand_;
+    }
+
+    bool get_stopped() const
+    {
+        return stopped_;
+    }
+
+    deadline::ptr get_timer()
+    {
+        return timer_;
+    }
+};
+
+BOOST_AUTO_TEST_CASE(connector__construct__default__expected_properties)
+{
+    threadpool pool(1);
+    asio::strand strand(pool.service().get_executor());
+    const settings set(bc::system::chain::selection::mainnet);
+    auto instance = std::make_shared<accessor>(strand, pool.service(), set);
+
+    BOOST_REQUIRE(&instance->get_settings() == &set);
+    BOOST_REQUIRE(&instance->get_service() == &pool.service());
+    BOOST_REQUIRE(&instance->get_strand() == &strand);
+    BOOST_REQUIRE(instance->get_stopped());
+    BOOST_REQUIRE(instance->get_timer());
+
+    pool.stop();
+    pool.join();
+}
+
+BOOST_AUTO_TEST_CASE(connector__connect1__timeout__channel_timeout)
+{
+    threadpool pool(2);
+    asio::strand strand(pool.service().get_executor());
+    settings set(bc::system::chain::selection::mainnet);
+    set.connect_timeout_seconds = 0;
+    auto instance = std::make_shared<connector>(strand, pool.service(), set);
+
+    boost::asio::post(strand, [&]()
+    {
+        instance->connect(config::endpoint{ "bogus.xxx", 42 }, [](const code& ec, channel::ptr channel)
+        {
+            BOOST_REQUIRE_EQUAL(ec, error::channel_timeout);
+            BOOST_REQUIRE(!channel);
+        });
+    });
+
+    pool.stop();
+    pool.join();
+}
+
+BOOST_AUTO_TEST_CASE(connector__connect2__timeout__channel_timeout)
+{
+    threadpool pool(2);
+    asio::strand strand(pool.service().get_executor());
+    settings set(bc::system::chain::selection::mainnet);
+    set.connect_timeout_seconds = 0;
+    auto instance = std::make_shared<connector>(strand, pool.service(), set);
+
+    boost::asio::post(strand, [&]()
+    {
+        instance->connect(config::authority{ "42.42.42.42", 42 }, [](const code& ec, channel::ptr channel)
+        {
+            BOOST_REQUIRE_EQUAL(ec, error::channel_timeout);
+            BOOST_REQUIRE(!channel);
+        });
+    });
+
+    pool.stop();
+    pool.join();
+}
+
+BOOST_AUTO_TEST_CASE(connector__connect3__timeout__channel_timeout)
+{
+    threadpool pool(2);
+    asio::strand strand(pool.service().get_executor());
+    settings set(bc::system::chain::selection::mainnet);
+    set.connect_timeout_seconds = 0;
+    auto instance = std::make_shared<connector>(strand, pool.service(), set);
+
+    boost::asio::post(strand, [&]()
+    {
+        instance->connect("bogus.xxx", 42, [](const code& ec, channel::ptr channel)
+        {
+            BOOST_REQUIRE_EQUAL(ec, error::channel_timeout);
+            BOOST_REQUIRE(!channel);
+        });
+    });
+
+    pool.stop();
+    pool.join();
+}
+
+BOOST_AUTO_TEST_CASE(connector__connect__stop__operation_canceled)
+{
+    threadpool pool(2);
+    asio::strand strand(pool.service().get_executor());
+    settings set(bc::system::chain::selection::mainnet);
+    set.connect_timeout_seconds = 1000;
+    auto instance = std::make_shared<connector>(strand, pool.service(), set);
+
+    boost::asio::post(strand, [&]()
+    {
+        instance->connect(config::endpoint{ "bogus.xxx", 42 }, [](const code& ec, channel::ptr channel)
+        {
+            // TODO: 11001 (HOST_NOT_FOUND) gets mapped to unknown.
+            BOOST_REQUIRE(ec == error::unknown || ec == error::operation_canceled);
+            BOOST_REQUIRE(!channel);
+        });
+
+        // Test race.
+        std::this_thread::sleep_for(microseconds(1));
+        instance->stop();
+    });
+
+    pool.stop();
+    pool.join();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
