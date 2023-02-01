2149d0e38a44685138dcb1240476e48f51014d09
evoskuil <eric@voskuil.org>
2023-01-31 17:10:45 -0800

    Log duplicate address request ignored.

--- src/protocols/protocol_address_31402.cpp
+++ src/protocols/protocol_address_31402.cpp
@@ -105,9 +105,11 @@ void protocol_address_31402::handle_receive_get_address(const code& ec,
     if (stopped(ec))
         return;
 
-    // TODO: log duplicate request (or drop channel).
     if (sent_)
+    {
+        LOG("Ignoring duplicate address request from [" << authority() << "]");
         return;
+    }
 
     fetches(BIND2(handle_fetch_addresses, _1, _2));
 }
@@ -121,8 +123,6 @@ void protocol_address_31402::handle_fetch_addresses(const code& ec,
         return;
 
     SEND1(address{ addresses }, handle_send, _1);
-
-    // Precludes multiple address requests.
     sent_ = true;
 }
 
