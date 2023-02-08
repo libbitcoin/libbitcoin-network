4993f9a0d651dafcaf62488d55c0603e7289e8ad
evoskuil <eric@voskuil.org>
2023-02-08 01:26:17 -0800

    Utilities::to_literal tests.

--- test/config/utilities.cpp
+++ test/config/utilities.cpp
@@ -18,11 +18,64 @@
  */
 #include "../test.hpp"
 
+using namespace config;
+
 BOOST_AUTO_TEST_SUITE(utilities_tests)
 
-BOOST_AUTO_TEST_CASE(utilities_test)
+// to_literal
+
+BOOST_AUTO_TEST_CASE(utilities__to_literal__empty_zero__empty)
 {
-    BOOST_REQUIRE(true);
+    BOOST_REQUIRE(to_literal("", 0).empty());
 }
+
+BOOST_AUTO_TEST_CASE(utilities__to_literal__single_token_zero__not_bracketed)
+{
+    BOOST_REQUIRE_EQUAL(to_literal("foobar", 0), "foobar");
+}
+
+BOOST_AUTO_TEST_CASE(utilities__to_literal__dotted_zero__not_bracketed)
+{
+    BOOST_REQUIRE_EQUAL(to_literal("foo.bar", 0), "foo.bar");
+}
+
+BOOST_AUTO_TEST_CASE(utilities__to_literal__coloned_zero__bracketed)
+{
+    BOOST_REQUIRE_EQUAL(to_literal("foo:bar", 0), "[foo:bar]");
+}
+
+BOOST_AUTO_TEST_CASE(utilities__to_literal__bracketed_coloned_zero__single_bracketed)
+{
+    BOOST_REQUIRE_EQUAL(to_literal("[foo:bar]", 0), "[foo:bar]");
+}
+
+BOOST_AUTO_TEST_CASE(utilities__to_literal__empty_nonzero__expected)
+{
+    BOOST_REQUIRE_EQUAL(to_literal("", 42), ":42");
+}
+
+BOOST_AUTO_TEST_CASE(utilities__to_literal__single_token_nonzero__not_bracketed_port)
+{
+    BOOST_REQUIRE_EQUAL(to_literal("foobar", 42), "foobar:42");
+}
+
+BOOST_AUTO_TEST_CASE(utilities__to_literal__dotted_nonzero__not_bracketed_port)
+{
+    BOOST_REQUIRE_EQUAL(to_literal("foo.bar", 42), "foo.bar:42");
+}
+
+BOOST_AUTO_TEST_CASE(utilities__to_literal__coloned_nonzero__bracketed_port)
+{
+    BOOST_REQUIRE_EQUAL(to_literal("foo:bar", 42), "[foo:bar]:42");
+}
+
+BOOST_AUTO_TEST_CASE(utilities__to_literal__bracketed_coloned_nonzero__single_bracketed_port)
+{
+    BOOST_REQUIRE_EQUAL(to_literal("[foo:bar]", 42), "[foo:bar]:42");
+}
+
+// to_host
+
+
 
 BOOST_AUTO_TEST_SUITE_END()
