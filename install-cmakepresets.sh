6241727b0037733379281b0f0abf8179e673a897
evoskuil <eric@voskuil.org>
2025-05-25 13:52:56 -0400

    Reduce boost dependencies, no nuget import headers, bump to 1.86.0.

--- install-cmakepresets.sh
+++ install-cmakepresets.sh
@@ -1058,13 +1058,10 @@ ICU_OPTIONS=(
 # Define boost options.
 #------------------------------------------------------------------------------
 BOOST_OPTIONS=(
-"--with-chrono" \
 "--with-iostreams" \
-"--with-json" \
 "--with-locale" \
 "--with-program_options" \
 "--with-regex" \
-"--with-system" \
 "--with-thread" \
 "--with-test")
 
