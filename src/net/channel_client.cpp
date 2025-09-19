dac3d045725eae69b98ceb9b58605443f452e1fc
evoskuil <eric@voskuil.org>
2025-09-19 01:04:10 -0400

    Iteratively increase rpc header buffer for deserialize.

--- src/net/channel_client.cpp
+++ src/net/channel_client.cpp
@@ -18,6 +18,8 @@
  */
 #include <bitcoin/network/net/channel_client.hpp>
 
+#include <algorithm>
+#include <utility>
 #include <bitcoin/network/async/async.hpp>
 #include <bitcoin/network/define.hpp>
 #include <bitcoin/network/log/log.hpp>
@@ -29,7 +31,9 @@ namespace libbitcoin {
 namespace network {
 
 using namespace system;
+using namespace messages::rpc;
 using namespace std::placeholders;
+using namespace std::ranges;
 
 BC_PUSH_WARNING(NO_THROW_IN_NOEXCEPT)
 
@@ -37,6 +41,7 @@ channel_client::channel_client(const logger& log, const socket::ptr& socket,
     const network::settings& settings, uint64_t identifier) NOEXCEPT
   : channel(log, socket, settings, identifier),
     distributor_(socket->strand()),
+    buffer_(min_heading),
     tracker<channel_client>(log)
 {
 }
@@ -90,18 +95,26 @@ void channel_client::read_request() NOEXCEPT
     if (stopped() || paused())
         return;
 
-    // TODO: dynamic or fixed.
-    buffer_.resize(messages::rpc::max_request);
-
-    // Post handle_read_heading to strand upon stop, error, or buffer full.
-    read_some(buffer_,
-        std::bind(&channel_client::handle_read_request,
-            shared_from_base<channel_client>(), _1, _2));
+    read_request(zero);
 }
 
-void channel_client::handle_read_request(const code& ec, size_t) NOEXCEPT
+void channel_client::read_request(size_t offset) NOEXCEPT
 {
     BC_ASSERT_MSG(stranded(), "strand");
+
+    // Post handle_read_heading to strand upon stop, error, or buffer full.
+    read_some({ std::next(buffer_.begin(), offset), buffer_.end() },
+        std::bind(&channel_client::handle_read_request,
+            shared_from_base<channel_client>(), _1, _2, offset));
+}
+
+void channel_client::handle_read_request(const code& ec, size_t bytes_read,
+    size_t offset) NOEXCEPT
+{
+    // Reliance on API correctness and protected invocation is sufficient.
+    BC_ASSERT_MSG(stranded(), "strand");
+    BC_ASSERT_MSG(!is_add_overflow(offset, bytes_read), "overflow");
+    BC_ASSERT_MSG(buffer_.size() >= add(offset, bytes_read), "buffer overflow");
 
     if (stopped())
     {
@@ -122,22 +135,55 @@ void channel_client::handle_read_request(const code& ec, size_t) NOEXCEPT
         return;
     }
 
-    const auto request = messages::rpc::request::deserialize(buffer_);
+    const auto size = offset + bytes_read;
+    const auto end = std::next(buffer_.cbegin(), size);
+    const auto populated = std::ranges::subrange(buffer_.cbegin(), end);
+    if (search(populated, heading::terminal).empty())
+    {
+        if (buffer_.size() == max_heading)
+        {
+            // TODO: notify subscribers with faulted request object.
+            LOGR("Request oversized header [" << authority() << "]");
+            stop(error::invalid_message);
+            return;
+        }
 
-    if (!request)
+        static_assert(max_heading < to_half(max_size_t));
+        buffer_.resize(limit(two * buffer_.size(), max_heading));
+        read_request(size);
+        return;
+    }
+
+    istream source{ buffer_ };
+    byte_reader reader{ source };
+    reader.set_limit(size);
+    const auto request = to_shared(request::deserialize(reader));
+    if (!reader)
     {
         // TODO: notify subscribers with faulted request object.
-        LOGR("Request invalid or oversized header [" << authority() << "]");
+        LOGR("Request invalid header [" << authority() << "]");
         stop(error::invalid_message);
         return;
     }
 
-    ////std::string out{};
-    ////out.resize(request->size());
-    ////if (request->serialize(out))
-    ////{
-    ////    LOGA(out);
-    ////}
+    // reader is positioned to first body byte and limited to read bytes.
+    // However more read is required and not limited to fixed buffer.
+    // Also a second request may potentially arrive at the end of a first when
+    // operating in full duplex mode, so we need to limit reads to predicted
+    // lengths, either via content-length or iteration over chunked encoding.
+    // We can reuse and increase the buffer size for each length/chunk, as
+    // required, and reset (trim) after body completion. But we want contiguous
+    // reads per chunk, cannot predict the request header length, and prefer to
+    // not copy the partial body read to the buffer start before reading after
+    // completing the header, since the body might already be read. So we can
+    // just resize up the buffer if required, but this invalidates iterators.
+
+    std::string out{};
+    out.resize(request->size());
+    if (request->serialize(out))
+    {
+        LOGA(out);
+    }
 }
 
 BC_POP_WARNING()
