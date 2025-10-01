8f44f4a351f75aa7bddeb160f07f04ffe8958f90
evoskuil <eric@voskuil.org>
2025-09-30 22:54:50 -0400

    Remove dead includes.

--- src/net/channel.cpp
+++ src/net/channel.cpp
@@ -18,7 +18,6 @@
  */
 #include <bitcoin/network/net/channel.hpp>
 
-#include <functional>
 #include <bitcoin/system.hpp>
 #include <bitcoin/network/async/async.hpp>
 #include <bitcoin/network/config/config.hpp>
