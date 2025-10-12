ea5c4d902f407db5de33c7a000948017078785c8
evoskuil <eric@voskuil.org>
2025-10-12 16:15:06 -0400

    Style, comments.

--- src/client.cpp
+++ src/client.cpp
@@ -25,6 +25,23 @@
 namespace libbitcoin {
 namespace network {
 
+// utility
+static system::string_list to_host_names(const config::endpoints& hosts,
+    bool secure) NOEXCEPT
+{
+    const auto port = secure ? http::default_tls : http::default_http;
+
+    system::string_list out{};
+    out.resize(hosts.size());
+    std::ranges::transform(hosts, out.begin(), [=](const auto& value) NOEXCEPT
+        {
+            return value.to_lower(port);
+        });
+
+    return out;
+}
+
+
 // tcp_server
 // ----------------------------------------------------------------------------
 
@@ -40,21 +57,6 @@ steady_clock::duration tcp_server::timeout() const NOEXCEPT
 
 // http_server
 // ----------------------------------------------------------------------------
-
-inline system::string_list to_host_names(const config::endpoints& hosts,
-    bool secure) NOEXCEPT
-{
-    const auto port = secure ? http::default_tls : http::default_http;
-
-    system::string_list out{};
-    out.resize(hosts.size());
-    std::ranges::transform(hosts, out.begin(), [=](const auto& value) NOEXCEPT
-    {
-        return value.to_lower(port);
-    });
-
-    return out;
-}
 
 system::string_list http_server::host_names() const NOEXCEPT
 {
