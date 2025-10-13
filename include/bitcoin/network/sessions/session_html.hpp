5dc70b661c5409cdc7814ead6a9e2b51b607ba92
evoskuil <eric@voskuil.org>
2025-10-13 00:54:40 -0400

    Fix include guard name.

--- include/bitcoin/network/sessions/session_html.hpp
+++ include/bitcoin/network/sessions/session_html.hpp
@@ -16,8 +16,8 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef LIBBITCOIN_NETWORK_SESSION_HTTP_HPP
-#define LIBBITCOIN_NETWORK_SESSION_HTTP_HPP
+#ifndef LIBBITCOIN_NETWORK_SESSION_HTML_HPP
+#define LIBBITCOIN_NETWORK_SESSION_HTML_HPP
 
 #include <memory>
 #include <bitcoin/network/define.hpp>
