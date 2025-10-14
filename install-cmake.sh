libbitcoin/libbitcoin-protocol

Regenerate artifacts.

install-cmake.sh
@@ -59,7 +59,7 @@ fi
 
 # The default build directory.
 #------------------------------------------------------------------------------
-BUILD_DIR="build-libbitcoin-protocol"
+BUILD_SRC_DIR="build-libbitcoin-protocol"
 
 PRESUMED_CI_PROJECT_PATH=$(pwd)
 
@@ -166,13 +166,13 @@ make_jobs()
     shift 1
 
     VERBOSITY=""
-    if [[ DISPLAY_VERBOSE ]]; then
+    if [[ $DISPLAY_VERBOSE ]]; then
         VERBOSITY="VERBOSE=1"
     fi
 
     SEQUENTIAL=1
     # Avoid setting -j1 (causes problems on single threaded systems [TRAVIS]).
-    if [[ $JOBS > $SEQUENTIAL ]]; then
+    if [[ $JOBS -gt $SEQUENTIAL ]]; then
         make -j"$JOBS" "$@" $VERBOSITY
     else
         make "$@" $VERBOSITY
@@ -278,7 +278,7 @@ parse_command_line_options()
             (--build-secp256k1)                        BUILD_SECP256K1="yes";;
 
             # Unique script options.
-            (--build-dir=*)         BUILD_DIR="${OPTION#*=}";;
+            (--build-dir=*)         BUILD_SRC_DIR="${OPTION#*=}";;
 
             # Handle ndebug declarations due to disabled argument passthrough
             (--enable-ndebug)       ENABLE_NDEBUG="yes";;
@@ -483,7 +483,7 @@ display_configuration()
     display_message "BUILD_BOOST           : $BUILD_BOOST"
     display_message "BUILD_SECP256K1       : $BUILD_SECP256K1"
     display_message "BOOST_ROOT            : $BOOST_ROOT"
-    display_message "BUILD_DIR                      : $BUILD_DIR"
+    display_message "BUILD_SRC_DIR                  : $BUILD_SRC_DIR"
     display_message "CUMULATIVE_FILTERED_ARGS       : $CUMULATIVE_FILTERED_ARGS"
     display_message "CUMULATIVE_FILTERED_ARGS_CMAKE : $CUMULATIVE_FILTERED_ARGS_CMAKE"
     display_message "PREFIX                : $PREFIX"
@@ -1065,10 +1065,10 @@ BITCOIN_PROTOCOL_OPTIONS=(
 display_configuration
 
 if [[ ! ($CI == true) ]]; then
-    create_directory "$BUILD_DIR"
-    push_directory "$BUILD_DIR"
+    create_directory "$BUILD_SRC_DIR"
+    push_directory "$BUILD_SRC_DIR"
 else
-    push_directory "$BUILD_DIR"
+    push_directory "$BUILD_SRC_DIR"
 fi
 
 initialize_git

install-cmakepresets.sh
@@ -65,7 +65,7 @@ fi
 
 # The default build directory.
 #------------------------------------------------------------------------------
-BUILD_DIR="build-libbitcoin-protocol"
+BUILD_SRC_DIR="build-libbitcoin-protocol"
 
 PRESUMED_CI_PROJECT_PATH=$(pwd)
 
@@ -172,13 +172,13 @@ make_jobs()
     shift 1
 
     VERBOSITY=""
-    if [[ DISPLAY_VERBOSE ]]; then
+    if [[ $DISPLAY_VERBOSE ]]; then
         VERBOSITY="VERBOSE=1"
     fi
 
     SEQUENTIAL=1
     # Avoid setting -j1 (causes problems on single threaded systems [TRAVIS]).
-    if [[ $JOBS > $SEQUENTIAL ]]; then
+    if [[ $JOBS -gt $SEQUENTIAL ]]; then
         make -j"$JOBS" "$@" $VERBOSITY
     else
         make "$@" $VERBOSITY
@@ -285,7 +285,7 @@ parse_command_line_options()
             (--build-secp256k1)                        BUILD_SECP256K1="yes";;
 
             # Unique script options.
-            (--build-dir=*)         BUILD_DIR="${OPTION#*=}";;
+            (--build-dir=*)         BUILD_SRC_DIR="${OPTION#*=}";;
             (--preset=*)            PRESET_ID="${OPTION#*=}";;
 
             # Handle ndebug declarations due to disabled argument passthrough
@@ -518,7 +518,7 @@ display_configuration()
     display_message "BUILD_BOOST           : $BUILD_BOOST"
     display_message "BUILD_SECP256K1       : $BUILD_SECP256K1"
     display_message "BOOST_ROOT            : $BOOST_ROOT"
-    display_message "BUILD_DIR                      : $BUILD_DIR"
+    display_message "BUILD_SRC_DIR                  : $BUILD_SRC_DIR"
     display_message "PRESET_ID                      : $PRESET_ID"
     display_message "CUMULATIVE_FILTERED_ARGS       : $CUMULATIVE_FILTERED_ARGS"
     display_message "CUMULATIVE_FILTERED_ARGS_CMAKE : $CUMULATIVE_FILTERED_ARGS_CMAKE"
@@ -1107,10 +1107,10 @@ BITCOIN_PROTOCOL_OPTIONS=(
 display_configuration
 
 if [[ ! ($CI == true) ]]; then
-    create_directory "$BUILD_DIR"
-    push_directory "$BUILD_DIR"
+    create_directory "$BUILD_SRC_DIR"
+    push_directory "$BUILD_SRC_DIR"
 else
-    push_directory "$BUILD_DIR"
+    push_directory "$BUILD_SRC_DIR"
 fi
 
 initialize_git

install.sh
@@ -59,7 +59,7 @@ fi
 
 # The default build directory.
 #------------------------------------------------------------------------------
-BUILD_DIR="build-libbitcoin-protocol"
+BUILD_SRC_DIR="build-libbitcoin-protocol"
 
 PRESUMED_CI_PROJECT_PATH=$(pwd)
 
@@ -166,13 +166,13 @@ make_jobs()
     shift 1
 
     VERBOSITY=""
-    if [[ DISPLAY_VERBOSE ]]; then
+    if [[ $DISPLAY_VERBOSE ]]; then
         VERBOSITY="VERBOSE=1"
     fi
 
     SEQUENTIAL=1
     # Avoid setting -j1 (causes problems on single threaded systems [TRAVIS]).
-    if [[ $JOBS > $SEQUENTIAL ]]; then
+    if [[ $JOBS -gt $SEQUENTIAL ]]; then
         make -j"$JOBS" "$@" $VERBOSITY
     else
         make "$@" $VERBOSITY
@@ -278,7 +278,7 @@ parse_command_line_options()
             (--build-secp256k1)                        BUILD_SECP256K1="yes";;
 
             # Unique script options.
-            (--build-dir=*)    BUILD_DIR="${OPTION#*=}";;
+            (--build-dir=*)    BUILD_SRC_DIR="${OPTION#*=}";;
         esac
     done
 }
@@ -428,7 +428,7 @@ display_configuration()
     display_message "BUILD_BOOST           : $BUILD_BOOST"
     display_message "BUILD_SECP256K1       : $BUILD_SECP256K1"
     display_message "BOOST_ROOT            : $BOOST_ROOT"
-    display_message "BUILD_DIR             : $BUILD_DIR"
+    display_message "BUILD_SRC_DIR          : $BUILD_SRC_DIR"
     display_message "PREFIX                : $PREFIX"
     display_message "DISABLE_SHARED        : $DISABLE_SHARED"
     display_message "DISABLE_STATIC        : $DISABLE_STATIC"
@@ -939,10 +939,10 @@ BITCOIN_PROTOCOL_OPTIONS=(
 display_configuration
 
 if [[ ! ($CI == true) ]]; then
-    create_directory "$BUILD_DIR"
-    push_directory "$BUILD_DIR"
+    create_directory "$BUILD_SRC_DIR"
+    push_directory "$BUILD_SRC_DIR"
 else
-    push_directory "$BUILD_DIR"
+    push_directory "$BUILD_SRC_DIR"
 fi
 
 initialize_git

libbitcoin/libbitcoin-network

install-cmake.sh
@@ -58,7 +58,7 @@ fi
 
 # The default build directory.
 #------------------------------------------------------------------------------
-BUILD_DIR="build-libbitcoin-network"
+BUILD_SRC_DIR="build-libbitcoin-network"
 
 PRESUMED_CI_PROJECT_PATH=$(pwd)
 
@@ -160,13 +160,13 @@ make_jobs()
     shift 1
 
     VERBOSITY=""
-    if [[ DISPLAY_VERBOSE ]]; then
+    if [[ $DISPLAY_VERBOSE ]]; then
         VERBOSITY="VERBOSE=1"
     fi
 
     SEQUENTIAL=1
     # Avoid setting -j1 (causes problems on single threaded systems [TRAVIS]).
-    if [[ $JOBS > $SEQUENTIAL ]]; then
+    if [[ $JOBS -gt $SEQUENTIAL ]]; then
         make -j"$JOBS" "$@" $VERBOSITY
     else
         make "$@" $VERBOSITY
@@ -270,7 +270,7 @@ parse_command_line_options()
             (--build-secp256k1)                        BUILD_SECP256K1="yes";;
 
             # Unique script options.
-            (--build-dir=*)         BUILD_DIR="${OPTION#*=}";;
+            (--build-dir=*)         BUILD_SRC_DIR="${OPTION#*=}";;
 
             # Handle ndebug declarations due to disabled argument passthrough
             (--enable-ndebug)       ENABLE_NDEBUG="yes";;
@@ -474,7 +474,7 @@ display_configuration()
     display_message "BUILD_BOOST           : $BUILD_BOOST"
     display_message "BUILD_SECP256K1       : $BUILD_SECP256K1"
     display_message "BOOST_ROOT            : $BOOST_ROOT"
-    display_message "BUILD_DIR                      : $BUILD_DIR"
+    display_message "BUILD_SRC_DIR                  : $BUILD_SRC_DIR"
     display_message "CUMULATIVE_FILTERED_ARGS       : $CUMULATIVE_FILTERED_ARGS"
     display_message "CUMULATIVE_FILTERED_ARGS_CMAKE : $CUMULATIVE_FILTERED_ARGS_CMAKE"
     display_message "PREFIX                : $PREFIX"
@@ -1041,10 +1041,10 @@ BITCOIN_NETWORK_OPTIONS=(
 display_configuration
 
 if [[ ! ($CI == true) ]]; then
-    create_directory "$BUILD_DIR"
-    push_directory "$BUILD_DIR"
+    create_directory "$BUILD_SRC_DIR"
+    push_directory "$BUILD_SRC_DIR"
 else
-    push_directory "$BUILD_DIR"
+    push_directory "$BUILD_SRC_DIR"
 fi
 
 initialize_git

install-cmakepresets.sh
@@ -64,7 +64,7 @@ fi
 
 # The default build directory.
 #------------------------------------------------------------------------------
-BUILD_DIR="build-libbitcoin-network"
+BUILD_SRC_DIR="build-libbitcoin-network"
 
 PRESUMED_CI_PROJECT_PATH=$(pwd)
 
@@ -166,13 +166,13 @@ make_jobs()
     shift 1
 
     VERBOSITY=""
-    if [[ DISPLAY_VERBOSE ]]; then
+    if [[ $DISPLAY_VERBOSE ]]; then
         VERBOSITY="VERBOSE=1"
     fi
 
     SEQUENTIAL=1
     # Avoid setting -j1 (causes problems on single threaded systems [TRAVIS]).
-    if [[ $JOBS > $SEQUENTIAL ]]; then
+    if [[ $JOBS -gt $SEQUENTIAL ]]; then
         make -j"$JOBS" "$@" $VERBOSITY
     else
         make "$@" $VERBOSITY
@@ -277,7 +277,7 @@ parse_command_line_options()
             (--build-secp256k1)                        BUILD_SECP256K1="yes";;
 
             # Unique script options.
-            (--build-dir=*)         BUILD_DIR="${OPTION#*=}";;
+            (--build-dir=*)         BUILD_SRC_DIR="${OPTION#*=}";;
             (--preset=*)            PRESET_ID="${OPTION#*=}";;
 
             # Handle ndebug declarations due to disabled argument passthrough
@@ -515,7 +515,7 @@ display_configuration()
     display_message "BUILD_BOOST           : $BUILD_BOOST"
     display_message "BUILD_SECP256K1       : $BUILD_SECP256K1"
     display_message "BOOST_ROOT            : $BOOST_ROOT"
-    display_message "BUILD_DIR                      : $BUILD_DIR"
+    display_message "BUILD_SRC_DIR                  : $BUILD_SRC_DIR"
     display_message "PRESET_ID                      : $PRESET_ID"
     display_message "CUMULATIVE_FILTERED_ARGS       : $CUMULATIVE_FILTERED_ARGS"
     display_message "CUMULATIVE_FILTERED_ARGS_CMAKE : $CUMULATIVE_FILTERED_ARGS_CMAKE"
@@ -1089,10 +1089,10 @@ BITCOIN_NETWORK_OPTIONS=(
 display_configuration
 
 if [[ ! ($CI == true) ]]; then
-    create_directory "$BUILD_DIR"
-    push_directory "$BUILD_DIR"
+    create_directory "$BUILD_SRC_DIR"
+    push_directory "$BUILD_SRC_DIR"
 else
-    push_directory "$BUILD_DIR"
+    push_directory "$BUILD_SRC_DIR"
 fi
 
 initialize_git

install.sh
@@ -58,7 +58,7 @@ fi
 
 # The default build directory.
 #------------------------------------------------------------------------------
-BUILD_DIR="build-libbitcoin-network"
+BUILD_SRC_DIR="build-libbitcoin-network"
 
 PRESUMED_CI_PROJECT_PATH=$(pwd)
 
@@ -160,13 +160,13 @@ make_jobs()
     shift 1
 
     VERBOSITY=""
-    if [[ DISPLAY_VERBOSE ]]; then
+    if [[ $DISPLAY_VERBOSE ]]; then
         VERBOSITY="VERBOSE=1"
     fi
 
     SEQUENTIAL=1
     # Avoid setting -j1 (causes problems on single threaded systems [TRAVIS]).
-    if [[ $JOBS > $SEQUENTIAL ]]; then
+    if [[ $JOBS -gt $SEQUENTIAL ]]; then
         make -j"$JOBS" "$@" $VERBOSITY
     else
         make "$@" $VERBOSITY
@@ -270,7 +270,7 @@ parse_command_line_options()
             (--build-secp256k1)                        BUILD_SECP256K1="yes";;
 
             # Unique script options.
-            (--build-dir=*)    BUILD_DIR="${OPTION#*=}";;
+            (--build-dir=*)    BUILD_SRC_DIR="${OPTION#*=}";;
         esac
     done
 }
@@ -419,7 +419,7 @@ display_configuration()
     display_message "BUILD_BOOST           : $BUILD_BOOST"
     display_message "BUILD_SECP256K1       : $BUILD_SECP256K1"
     display_message "BOOST_ROOT            : $BOOST_ROOT"
-    display_message "BUILD_DIR             : $BUILD_DIR"
+    display_message "BUILD_SRC_DIR          : $BUILD_SRC_DIR"
     display_message "PREFIX                : $PREFIX"
     display_message "DISABLE_SHARED        : $DISABLE_SHARED"
     display_message "DISABLE_STATIC        : $DISABLE_STATIC"
@@ -915,10 +915,10 @@ BITCOIN_NETWORK_OPTIONS=(
 display_configuration
 
 if [[ ! ($CI == true) ]]; then
-    create_directory "$BUILD_DIR"
-    push_directory "$BUILD_DIR"
+    create_directory "$BUILD_SRC_DIR"
+    push_directory "$BUILD_SRC_DIR"
 else
-    push_directory "$BUILD_DIR"
+    push_directory "$BUILD_SRC_DIR"
 fi
 
 initialize_git