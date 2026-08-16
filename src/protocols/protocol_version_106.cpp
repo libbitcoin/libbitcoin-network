21205b1230ff23d6b0472b42332115b3cc40b781
Eric Voskuil <eric@voskuil.org>
2026-08-16 03:23:56 -0400

    Align clear/private in p2p/s connect log message.

--- src/protocols/protocol_version_106.cpp
+++ src/protocols/protocol_version_106.cpp
@@ -309,7 +309,7 @@ bool protocol_version_106::handle_receive_version(const code& ec,
     ////    "/BitcoinFinance:");
 
     LOG_ONLY(const auto prefix = (inbound_ ? "Inbound" : "Outbound");)
-    LOG_ONLY(const auto security = (encrypted() ? "private" : "clear");)
+    LOG_ONLY(const auto security = (encrypted() ? "private" : "-clear-");)
     LOGN(prefix << " " << security << " [" << opposite() << "] version ("
         << message->value << ") " << user_agent);
 
