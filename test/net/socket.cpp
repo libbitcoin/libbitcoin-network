50daf84c6c12a22fbe9cbe69bb90d778bb740d41
Eric Voskuil <eric@voskuil.org>
2026-09-18 22:29:59 -0400

    Fix test break.

--- test/net/socket.cpp
+++ test/net/socket.cpp
@@ -64,8 +64,7 @@ BOOST_AUTO_TEST_CASE(socket__construct__default__closed_not_stopped_expected)
     BOOST_REQUIRE(instance->get_address() == instance->address());
     BOOST_REQUIRE(instance->get_address() == config::address{});
     BOOST_REQUIRE_EQUAL(instance->get_maximum_request(), maximum);
-    BOOST_REQUIRE(instance->binding().host().empty());
-    BOOST_REQUIRE_EQUAL(instance->binding().port(), 0u);
+    BOOST_REQUIRE(instance->binding() == config::endpoint{});
     instance->stop();
 }
 
