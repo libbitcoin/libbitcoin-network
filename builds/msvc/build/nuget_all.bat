85864d62a00cffddae01d5cdf38e89c88d161917
Eric Voskuil <eric@voskuil.org>
2026-09-22 17:10:07 -0400

    Remove dead code.

--- builds/msvc/build/nuget_all.bat
+++ builds/msvc/build/nuget_all.bat
@@ -1,13 +1,4 @@
 @ECHO OFF
-ECHO Downloading libbitcoin vs2022 dependencies from NuGet
-CALL nuget.exe install ..\vs2022\libbitcoin-network\packages.config
-CALL nuget.exe install ..\vs2022\libbitcoin-network-test\packages.config
-ECHO.
-ECHO Downloading libbitcoin vs2019 dependencies from NuGet
-CALL nuget.exe install ..\vs2019\libbitcoin-network\packages.config
-CALL nuget.exe install ..\vs2019\libbitcoin-network-test\packages.config
-ECHO.
-ECHO Downloading libbitcoin vs2017 dependencies from NuGet
-CALL nuget.exe install ..\vs2017\libbitcoin-network\packages.config
-CALL nuget.exe install ..\vs2017\libbitcoin-network-test\packages.config
-
+ECHO Downloading libbitcoin vs2026 dependencies from NuGet
+CALL nuget.exe install ..\vs2026\libbitcoin-network\packages.config
+CALL nuget.exe install ..\vs2026\libbitcoin-network-test\packages.config
